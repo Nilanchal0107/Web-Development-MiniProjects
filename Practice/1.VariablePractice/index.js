246cc27d361cd07b449fe3bbd08c81ea77288313
Nilanchal Jena <nilanchaljena2702@gmail.com>
2026-01-09 03:42:33 +0530

    Add comments for variable practice clarity
    Added comments to clarify variable declarations and concatenation.

--- Practice/1.VariablePractice/index.js
+++ Practice/1.VariablePractice/index.js
@@ -1,6 +1,9 @@
+// Create two variables, firstName and lastName
 let firstName = "Nilanchal"
 let lastName = "Jena"
 
+// Concatenate the two variables into a third variable called fullName
 let fullName = firstName + " " + lastName
 
+// Log fullName to the console
 console.log(fullName)
